fd32e763ecb7244574ea838cc346fef470cfd3fe
Olga Snyushkina <senyushkina.o@mail.ru>
2019-11-22 01:40:36 +0300

    update with pipeline

--- task2.js
+++ task2.js
@@ -1,23 +1,14 @@
-const csv = require('csvtojson');
-const readline = require('readline');
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import csv from "csvtojson";
+
 const filePath = "./node_mentoring_t1_2_input_example.csv";
 
-const readStream = require('fs').createReadStream(filePath);
-const writeStream = require('fs').createWriteStream("homework.txt");
+const readStream = createReadStream(filePath);
+const writeStream = createWriteStream("homework.txt");
 
-const rl = readline.createInterface({
-    input: readStream
-});
-
-let header = null;
-rl.on("line", line => {
-    if (!header) {
-        header = line.toLowerCase();
-    } else {
-        csv({
-            includeColumns: /book|author|price/
-        })
-            .fromString(header + "\n" + line)
-            .pipe(writeStream);
-    }    
-});
+pipeline(
+    readStream,
+    csv({ includeColumns: /book|author|price/i }),
+    writeStream
+)
